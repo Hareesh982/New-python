04e51be4d607fc784cbd234ff4b1a19365b3d511
D Hareesh <hareeshduddupudi@gmail.com>
2024-05-03 20:18:02 +0530

    Added Empty Functions

---
+++ main.py
@@ -0,0 +1,40 @@
+def Registration():
+    pass
+
+def Login():
+    pass
+
+def Profile():
+    pass
+
+def MyPosts():
+    pass
+
+def PasswordError():
+    pass
+
+def EmailError():
+    pass
+
+def Post():
+    pass
+
+def UpdatePassword():
+    pass
+
+def UpdateEmail():
+    pass
+
+def PostsCount():
+    pass
+
+def ForgotPassword():
+    pass
+
+def EmailMessage():
+    pass
+
+if __name__ == "__main__":
+    pass
+
+
