4f8db61c18a21969873e16a8aa5e1c0baa2485fa
D Hareesh <hareeshduddupudi@gmail.com>
2024-05-04 13:01:25 +0530

    implemented Registration function

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import json
+
 def Registration():
-    pass
+    name = input("Enter the name : ")
+    email = input("Enter Email : ")
+    password = input("Enter the password : ")
+    re_enter = input("Re-enter the password : ")
+
+    if len(password) < 8:
+        return "Length of the password should be more than 8"
+
+    if password != re_enter:
+        return "Password does not match"
+    
+    if "@" not in email or "." not in email:
+        return "Please enter a valid email"
+
+    with open("main.json",'r') as read_file:
+        data = json.load(read_file)
+
+    new_data = {
+        "user_name" : name,
+        "user_email" : email,
+        "user_password" : password
+    }
+
+    data.append(new_data)
+
+    with open("main.json",'w') as file:
+        json.dump(data,file,indent=4)
 
 def Login():
     pass
@@ -35,6 +63,6 @@ def EmailMessage():
     pass
 
 if __name__ == "__main__":
-    pass
+    print(Registration())
 
 
